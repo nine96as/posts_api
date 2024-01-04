b5af9a9dd6072e59f0699e62bc76405b6736b4d7
nine96as <nine96@skiff.com>
2024-01-04 11:59:59 +0000

    Tweak port to 3000

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from application import app
 from application import routes
+from application.posts import routes
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(port=3000, debug=True)
