0ef92add93f1a4e30b62300899df4962c1b3e111
nine96as <nine96@skiff.com>
2024-01-04 11:14:35 +0000

    Complete Post model logic with helper methods

---
+++ application/posts/models.py
@@ -0,0 +1,30 @@
+from application import db, app
+from sqlalchemy.sql import func
+
+app.app_context().push()
+
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.String(500), nullable=False)
+    created_at = db.Column(db.DateTime, default=func.now())
+    last_modified = db.Column(db.DateTime, onupdate=func.utc_timestamp())
+
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+    def __repr__(self) -> str:
+        return f'Post(id: {self.id}, title: {self.name})'
+
+    @property
+    def json(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "content": self.content,
+            "created_at": self.created,
+            "last_modified": self.last_modified
+        }
