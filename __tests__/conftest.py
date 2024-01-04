9fd127817a49bebfe720982b0981ddff3be1847a
nine96as <nine96@skiff.com>
2024-01-04 15:44:44 +0000

    Tweak tests to pass updated Post logic

--- __tests__/conftest.py
+++ __tests__/conftest.py
@@ -4,5 +4,5 @@ from application.posts.models import Post
 
 @pytest.fixture(scope='module')
 def new_post():
-    post = Post('adipisicing', 'Sint proident anim esse excepteur sint ut eiusmod deserunt velit fugiat consectetur. Laborum officia consectetur ipsum ullamco irure velit occaecat. Exercitation eu ipsum excepteur ut elit commodo ea eiusmod nostrud et ex. Eiusmod exercitation mollit dolore. Laborum cillum cillum ullamco enim esse.')
+    post = Post('adipisicing', 'Sint proident anim esse excepteur sint ut eiusmod deserunt velit fugiat consectetur. Laborum officia consectetur ipsum ullamco irure velit occaecat. Exercitation eu ipsum excepteur ut elit commodo ea eiusmod nostrud et ex. Eiusmod exercitation mollit dolore. Laborum cillum cillum ullamco enim esse.', user_id=1)
     return post
--- __tests__/unit/models/test_posts.py
+++ __tests__/unit/models/test_posts.py
@@ -8,11 +8,12 @@ def test_new_post_with_fixture(new_post):
     """
     assert new_post.title == 'adipisicing'
     assert new_post.content == 'Sint proident anim esse excepteur sint ut eiusmod deserunt velit fugiat consectetur. Laborum officia consectetur ipsum ullamco irure velit occaecat. Exercitation eu ipsum excepteur ut elit commodo ea eiusmod nostrud et ex. Eiusmod exercitation mollit dolore. Laborum cillum cillum ullamco enim esse.'
-    assert new_post.__repr__() == f'Post(id: {new_post.id}, title: {new_post.title})'
+    assert new_post.__repr__() == f'Post(id: {new_post.id}, title: {new_post.title}, user_id: {new_post.user_id})'
     assert new_post.json == {
         "id": new_post.id,
         "title": new_post.title,
         "content": new_post.content,
         "created_at": new_post.created_at,
-        "last_modified": new_post.last_modified
+        "last_modified": new_post.last_modified,
+        "user_id": new_post.user_id
     }
